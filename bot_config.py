6781d666ac4efae312b105ad8768fd803c1afc8c
IRONKAGE <IRONKAGE@gmail.com>
2020-06-30 16:07:47 +0300

    add Bot Config

---
+++ bot_config.py
@@ -0,0 +1,16 @@
+TOKEN = 'BotID'
+
+#ID Telegram Group
+ID1_Name_Team = '-ChatID'
+ID2_Name_Group = '-ChatID'
+ID3_Test = '-ChatID'
+
+#User in Group
+Users_Name_Team = '@Ivan\_FairyTails @Unknown'
+Users_Name_Group = '@SuperMan @BatMan'
+Users_Test = '@Cezar @Zeus\_001'
+
+#Link for Meet
+Link_Name_Team = 'https://meet.google.com/3456666GangStar35'
+Link_Name_Group = 'https://us04web.zoom.us/j/7777777?pwd=inJustice793155555'
+Link_Test = 'on Skype'
